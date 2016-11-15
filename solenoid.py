13f5369dc7121dbfda490b7f2521ae2b8abfd8f4
Ben Greenberg <nebbles@users.noreply.github.com>
2016-11-15 11:08:25 +0000

    Clean up solenoid.py, added documentation

--- solenoid.py
+++ solenoid.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
 
+'''
+solenoid.py is a test file for running the solenoid set up. Circuit diagram can be found in the same directory. Pins are set up as follows:
 
-# External module imports GPIO
-import RPi.GPIO as GPIO
-# Library to slow or give a rest to the script
-import time
+GPIO Pin    Purpose         Connected to
+1           3.3v power      Top pin (C) of transistor (see CBE transistor pins - European)
+6           GRND            Grnd of breadboard
+11          Input port      Button press
+16          Output port     Middle pin of transitor (open circuit for LED/Solenoid activation)
+
+'''
+
+import RPi.GPIO as GPIO # External module imports GPIO
+import time # Library to slow or give a rest to the script
 
 
 # Pin definiton using Broadcom scheme
-# Led
-solenoidpin = 23  # Broadcom pin 23 (P1 pin 16)
-# Button
-butPin = 17  # Broadcom pin 17 (P1 pin 11)
-
+solenoidpin = 23  # LED/Solenoid -- Broadcom pin 23 (P1 pin 16)
+butPin = 17  # BUTTON -- Broadcom pin 17 (P1 pin 11)
 
 # Pin Setup:
 GPIO.setmode(GPIO.BCM)  # Broadcom pin-numbering scheme
 GPIO.setup(solenoidpin, GPIO.OUT)  # LED pin set as output
-# Button pin set as input w/ pull-up resistors
-GPIO.setup(butPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(butPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up resistors
 
-flag = True
+flag = True # Flag to prevent looping print statement
 
 print("Press button to activate solenoid. Press CTRL+C to exit")
 try:
