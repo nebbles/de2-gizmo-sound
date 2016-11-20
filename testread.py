9c622b17fae6f05398e8d2e6b4a6e7fd5c8040b2
nebbles <bsg115@ic.ac.uk>
2016-11-20 17:01:03 +0000

    fixed testread file

--- testread.py
+++ testread.py
@@ -6,7 +6,6 @@ tune = [line.rstrip('\n') for line in infile]
 infile.close() # Close the filehandle
 
 print tune
-print lines
-print lines[4][0]
-print lines[4][1]
-print lines[4][2]
+print tune[4][0]
+print tune[4][1]
+print tune[4][2]
