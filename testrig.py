b3a0caa523636deaa1d50c5457b0dcaad7976a68
nebbles <bsg115@ic.ac.uk>
2016-11-20 23:15:46 +0000

    added program parser

--- testrig.py
+++ testrig.py
@@ -9,13 +9,44 @@ GPIO Pin    Purpose         Connected to
 13          Output          LED 2
 15          Output          LED 3
 
+Program currently supports 2 arguments.
+- Pass 'loop' to program if you want to loop the file.
+- Pass '<filename>' to program if a file other than 'example.py' needs to be used.
+
 '''
 
 import RPi.GPIO as GPIO # External module imports GPIO
 import time # Library to slow or give a rest to the script
+import sys # Library to access program arguments
+import timeit
+
+start_time = timeit.default_timer()
+elapsed = timeit.default_timer() - start_time
+
+shouldLoop = False
+args = sys.argv[1:]
+timeinterval = 0.5
+inputfile = 'example.py'
+
+if len(args) > 2:
+    print "Program can only accept 2 arguments:"
+    print "1: 'loop'"
+    print "2: <filename>"
+    sys.exit()
+
+for arg in args:
+    if arg == 'help':
+        print "Program can accept 2 arguments:"
+        print "1: 'loop'"
+        print "2: <filename>"
+        sys.exit()
+    elif arg == 'loop':
+        shouldLoop = True
+    else:
+        inputfile = arg
 
 ## -- IMPORT TUNE FROM FILE -- ##
-infile = open("example.txt", "r") # Open data file -- "r" is for read
+infile = open(inputfile, "r") # Open data file -- "r" is for read
 tune = [line.rstrip('\n') for line in infile]
 infile.close() # Close the filehandle
 
@@ -30,65 +61,41 @@ GPIO.setup(led1, GPIO.OUT)  # LED pin set as output
 GPIO.setup(led2, GPIO.OUT)  # LED pin set as output
 GPIO.setup(led3, GPIO.OUT)  # LED pin set as output
 
-# tune = [[1,0,0],\
-#         [1,1,0],\
-#         [1,0,0],\
-#         [1,1,1],\
-#         [1,0,0],\
-#         [1,1,0],\
-#         [1,0,0],\
-#         [1,1,1],\
-#         [1,0,0],\
-#         [1,1,0],\
-#         [1,0,0],\
-#         [1,1,1],\
-#         [1,0,0],\
-#         [1,1,0],\
-#         [1,0,0],\
-#         [1,1,1]]
+# Start up feedback
+print("LED Test Rig. Press CTRL+C to exit")
+if shouldLoop:
+    loopstatement = "Program will loop."
+else:
+    loopstatement = "Program will not loop and will exit upon completion."
+print(loopstatement)
+filestatement = "Tune being read from: " + inputfile
 
 
-print("LED Test Rig. Example file will play repetitively until exit. Press CTRL+C to exit")
-t = 0.5
-index = 0
-loop = 1
 try:
-    while True:
+    while shouldLoop:
+        for index in range(0,len(tune)):
+            start_time = timeit.default_timer()
 
-        # if index == len(tune):
-        #     index = 0
+            hit1, hit2, hit3 = False, False, False
+            notes = tune[index]
+            print index, " ", notes
 
-        print index
-        hit1, hit2, hit3 = False, False, False
+            if notes[0] == '1': hit1 = True
+            if notes[1] == '1': hit2 = True
+            if notes[2] == '1': hit3 = True
 
-        notes = tune[index]
-        print notes
-        if notes[0] == '1':
-            print 'hit1 true'
-            hit1 = True
-        if notes[1] == '1':
-            print 'hit2 true'
-            hit2 = True
-        if notes[2] == '1':
-            print 'hit3 true'
-            hit3 = True
+            if hit1: GPIO.output(led1, GPIO.HIGH)
+            if hit2: GPIO.output(led2, GPIO.HIGH)
+            if hit3: GPIO.output(led3, GPIO.HIGH)
 
-        if hit1:
-            GPIO.output(led1, GPIO.HIGH)
-        if hit2:
-            GPIO.output(led2, GPIO.HIGH)
-        if hit3:
-            GPIO.output(led3, GPIO.HIGH)
+            elapsed = timeit.default_timer() - start_time
+            print elapsed
+            
+            time.sleep(timeinterval)
 
-        time.sleep(t)
-        index += 1
-
-        GPIO.output(led1, GPIO.LOW)
-        GPIO.output(led2, GPIO.LOW)
-        GPIO.output(led3, GPIO.LOW)
-
-
-
+            GPIO.output(led1, GPIO.LOW)
+            GPIO.output(led2, GPIO.LOW)
+            GPIO.output(led3, GPIO.LOW)
 
 
 except KeyboardInterrupt:  # If CTRL+C is pressed, exit cleanly:
