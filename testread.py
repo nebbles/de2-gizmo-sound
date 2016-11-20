ef6443be93db04a83aa59d16b4b655c3e6bd966e
nebbles <bsg115@ic.ac.uk>
2016-11-20 16:45:11 +0000

    added testread file

---
+++ testread.py
@@ -0,0 +1,16 @@
+# test file
+
+# Open data file
+infile = open("example.txt", "r") # "r" is for read
+
+# Initialise empty lists
+tune = []
+
+# Loop through infile and write to x and y lists
+for line in infile:
+    tune.append(string(line))
+
+# Close the filehandle
+infile.close()
+
+print tune
