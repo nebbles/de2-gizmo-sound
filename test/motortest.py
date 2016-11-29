32b90599f370ff9c5bf8805b5c0bc34690f9b95e
nebbles <bsg115@ic.ac.uk>
2016-11-29 15:40:50 +0000

    added motortest.py

---
+++ test/motortest.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# motor test program
+
+import RPi.GPIO as GPIO # External module imports GPIO
+import time # Library to slow or give a rest to the script
+import sys # Library to access program arguments
+import timeit
+from __future__ import print_function
+#print(os.path.getsize(file_name)/1024+'KB / '+size+' KB downloaded!', end='\r')
+
+def startup():
+    startup_message = "motortest.py running..."
+    print startup_message
+    print "This program uses the MotorSystem and ControlSystem circuits."
+    print "Connect GPIO pin 16 to microswitch"
+    print "Connect GPIO pin 18 to LED"
+    print "Connect GPIO pin 12 to transistor gate"
+    answer = input("[Y/N] to confirm / quit")
+    if answer == 'n':
+        sys.exit()
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(23, GPIO.IN) # Microswitch pin 16 for feedback
+    GPIO.setup(24, GPIO.OUT) # LED pin 18 for feedback
+    GPIO.setup(18, GPIO.OUT) # Motor pin 12
+    motorpwm=GPIO.PWM(18,100)
+    return motorpwm
+
+startup()
+motorpwm = setup()
+motorpwm.start(0)
+while(1):
+    cycle=input("How fast? (20-100)")
+    motorpwm.ChangeDutyCycle(cycle)
+
+finally:
+    motorpwm.stop()
+    GPIO.cleanup()
