617414c1c49b1487363a9f68d30a173fdf92bf03
nebbles <bsg115@ic.ac.uk>
2016-11-29 15:45:39 +0000

    added try block to motortest.py

--- test/motortest.py
+++ test/motortest.py
@@ -9,31 +9,32 @@ import timeit
 from __future__ import print_function
 #print(os.path.getsize(file_name)/1024+'KB / '+size+' KB downloaded!', end='\r')
 
-def startup():
-    startup_message = "motortest.py running..."
-    print("motortest.py running...")
-    print("This program uses the MotorSystem and ControlSystem circuits.")
-    print("Connect GPIO pin 16 to microswitch")
-    print("Connect GPIO pin 18 to LED")
-    print("Connect GPIO pin 12 to transistor gate")
-    answer = input("[Y/N] to confirm / quit")
-    if answer == 'n':
-        sys.exit()
+try:
+    def startup():
+        startup_message = "motortest.py running..."
+        print("motortest.py running...")
+        print("This program uses the MotorSystem and ControlSystem circuits.")
+        print("Connect GPIO pin 16 to microswitch")
+        print("Connect GPIO pin 18 to LED")
+        print("Connect GPIO pin 12 to transistor gate")
+        answer = input("[Y/N] to confirm / quit")
+        if answer == 'n':
+            sys.exit()
 
-def setup():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(23, GPIO.IN) # Microswitch pin 16 for feedback
-    GPIO.setup(24, GPIO.OUT) # LED pin 18 for feedback
-    GPIO.setup(18, GPIO.OUT) # Motor pin 12
-    motorpwm=GPIO.PWM(18,100)
-    return motorpwm
+    def setup():
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(23, GPIO.IN) # Microswitch pin 16 for feedback
+        GPIO.setup(24, GPIO.OUT) # LED pin 18 for feedback
+        GPIO.setup(18, GPIO.OUT) # Motor pin 12
+        motorpwm=GPIO.PWM(18,100)
+        return motorpwm
 
-startup()
-motorpwm = setup()
-motorpwm.start(0)
-while(1):
-    cycle=input("How fast? (20-100)")
-    motorpwm.ChangeDutyCycle(cycle)
+    startup()
+    motorpwm = setup()
+    motorpwm.start(0)
+    while(1):
+        cycle=input("How fast? (20-100)")
+        motorpwm.ChangeDutyCycle(cycle)
 
 finally:
     motorpwm.stop()
