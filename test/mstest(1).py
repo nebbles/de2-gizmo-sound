9bba09902e53dc229a851ec8af60a25843e34a45
nebbles <bsg115@ic.ac.uk>
2016-12-01 20:35:34 +0000

    bug fixes from removing functions on microswitch tests

--- test/mstest(1).py
+++ test/mstest(1).py
@@ -9,16 +9,16 @@ import timeit
 #from __future__ import print_function
 #print(os.path.getsize(file_name)/1024+'KB / '+size+' KB downloaded!', end='\r')
 
-def startup():
-    startup_message = "motortest.py running..."
-    print("motortest.py running...")
-    print("This program uses the MotorSystem and ControlSystem circuits.")
-    print("Connect GPIO pin 16 to microswitch")
-    print("Connect GPIO pin 18 to LED")
-    print("Connect GPIO pin 12 to transistor gate")
-    answer = input("[Y/N] to confirm / quit")
-    if answer == 'n':
-        sys.exit()
+
+# startup_message = "motortest.py running..."
+# print("motortest.py running...")
+# print("This program uses the MotorSystem and ControlSystem circuits.")
+# print("Connect GPIO pin 16 to microswitch")
+# print("Connect GPIO pin 18 to LED")
+# print("Connect GPIO pin 12 to transistor gate")
+# answer = input("[Y/N] to confirm / quit")
+# if answer == 'n':
+#     sys.exit()
 
 
 GPIO.setmode(GPIO.BCM)
@@ -26,11 +26,8 @@ GPIO.setup(24, GPIO.OUT) # LED pin 18 for feedback
 GPIO.setup(18, GPIO.OUT) # Motor pin 12
 msPin = GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Microswitch pin 16 for feedback
 motorpwm = GPIO.PWM(18,100)
-return motorpwm
 
 try:
-    #startup()
-    motorpwm = setup()
     motorpwm.start(0)
     cycle=input("How fast? (20-100)")
     motorpwm.ChangeDutyCycle(cycle)
